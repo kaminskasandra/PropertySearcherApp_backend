d332a14634c4b1f728e8dcd7cf910d658d7f2ff3
kaminskasandra <san.porombka@gmail.com>
2024-05-10 14:26:19 +0200

    Creating new backend project

---
+++ src/main/java/com/propertysearcherprojectbackend/PropertySearcherProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.propertysearcherprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertySearcherProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertySearcherProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propertysearcherprojectbackend/PropertySearcherProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertysearcherprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySearcherProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

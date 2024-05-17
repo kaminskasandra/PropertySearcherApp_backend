7c5e4f5e18d1bdd7d480eb62ddaae4e976fa346f
kaminskasandra <san.porombka@gmail.com>
2024-05-17 11:30:50 +0200

    EmailService fixed

---
+++ src/test/java/com/example/propertysearcherprojectbackend/service/EmailServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.propertysearcherprojectbackend.service;
+
+import com.example.propertysearcherprojectbackend.domain.Mail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class EmailServiceTest {
+    @InjectMocks
+    private EmailService emailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail() {
+        //Given
+        Mail mail = new Mail("test@test.com", "Test", "Test Message");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        //When
+        emailService.send(mail);
+        //Then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+}
